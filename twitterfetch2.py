587006337feea48f96997c0fe0f6354116f1b87a
Syreus868389 <65856915+Syreus868389@users.noreply.github.com>
2020-12-01 19:08:39 +0100

    beta 1

--- twitterfetch2.py
+++ twitterfetch2.py
@@ -2,7 +2,21 @@ import tweepy
 import pandas as pd
 import csv
 import time
+from os import path, getcwd
 from collections import Counter
+from nltk.corpus import stopwords
+import numpy as np
+from PIL import Image 
+import matplotlib 
+matplotlib.use('AGG')
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+stop_words = set(stopwords.words('french'))
+
+d = getcwd()
+image = Image.open(path.join(d, "Twitter-Logo.png"))
+mask = np.array(image)
 
 consumer_key="x5ER0kkhEvbS8cJg8jDF2Z04T"
 consumer_secret="6WSZoraAH8CIMqkijT04OkdFSQ42LvoRSp7FFPmNTKT0h26fSR"
@@ -16,23 +30,51 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 def fetch_tweets(query):
-    tweets = []
     ids = []
+    tweet_corpus = []
     results = tweepy.Cursor(api.search, q=query+'-filter:retweets', lang='fr').items(300)
-    while len(ids) < 100:
-        for status in results:
-            id = status.user.id
-            ids.append(id)
-            print(len(ids))
-            print(ids)
+    for status in results:
+        id = status.user.id
+        ids.append(id)
+        if len(ids) < 100:
+            continue
+        else:
+            break
     
     for id in ids:
         statuses = tweepy.Cursor(api.user_timeline, user_id=id, tweet_mode='extended', include_rts=False).items(20)
         for status in statuses:
             tweet = status.full_text
-            tweets.append(tweet)
-    return tweets
+            tweet_words = tweet.split(" ")
+            for word in tweet_words:
+                if word not in stop_words:
+                    tweet_corpus.append(word)
+    tweet_count = Counter(tweet_corpus)
+    
+    df = pd.DataFrame(list(tweet_count.items()),columns=['Tweets','Count'])
+    sorted_df = df.sort_values(by='Count', ascending=False)
+    sorted_df.to_excel(query + '.xlsx')
+    
+    delete_keys = []
+    for key,value in tweet_count.items():
+        if value < 10:
+           delete_keys.append(key)
+        else:
+            continue
+    for key in delete_keys:
+        tweet_count.pop(key)
+    
+    wordcloud = WordCloud(background_color="white", max_words=200, mask=mask).generate_from_frequencies(tweet_count)
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.savefig(query + '.png')
+    
+fetch_tweets('TXT')  
+            
 
-TXTtweets = fetch_tweets('TXT')
+
+
+
+
 
 
