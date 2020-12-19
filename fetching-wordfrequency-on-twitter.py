ce457113a9a624a961f24325703a4cb670cf0d6e
Syreus868389 <65856915+Syreus868389@users.noreply.github.com>
2020-12-19 16:55:43 +0100

    hide keys

--- fetching-wordfrequency-on-twitter.py
+++ fetching-wordfrequency-on-twitter.py
@@ -20,11 +20,11 @@ d = getcwd()
 image = Image.open(path.join(d, "Twitter-Logo.png"))
 mask = np.array(image)
 
-consumer_key="x5ER0kkhEvbS8cJg8jDF2Z04T"
-consumer_secret="6WSZoraAH8CIMqkijT04OkdFSQ42LvoRSp7FFPmNTKT0h26fSR"
+consumer_key="*****************"
+consumer_secret="**************"
 
-access_token="1224316478218014722-pUGe6PQPmiN8lzzwJc4Kmb2XGnxudh"
-access_token_secret="BcLOFyeR6ewEuHWfelAjqtIGS64tH5zZxT9EHx5EDjJ8d"
+access_token="******************"
+access_token_secret="*********************"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
